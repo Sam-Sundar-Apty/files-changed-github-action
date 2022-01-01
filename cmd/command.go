9cd5588100f8512fffc42c863d06097c310c87af
saiumesh535 <d.sai535@gmail.com>
2022-01-01 20:48:12 +0530

    added comment

--- cmd/command.go
+++ cmd/command.go
@@ -1,13 +1,12 @@
 package cmd
 
 import (
-	"fmt"
 	"os/exec"
 
 	"github.com/actions-go/toolkit/core"
 )
 
 func RunCommand(command string) ([]byte, error) {
-	core.Info(fmt.Sprintf("running command %s", command))
+	core.Info("running....")
 	return exec.Command("git", "diff", "development", "feature/LSP-0000/checking-if-files-changed", "--", "./scripts", "./scripts").Output()
 }
